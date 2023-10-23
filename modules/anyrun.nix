355047ff29c6df47df91bf183d60fdf5e8abc18b
FredM7 <fred@moller.cloud>
2023-10-23 16:47:27 +0200

    anyrun styling

--- modules/anyrun.nix
+++ modules/anyrun.nix
@@ -8,10 +8,33 @@
     config = {
       #width = { fraction = 0.3; };
 			#position = "top";
-			plugins = [];
+			closeOnClick = true;
+			x = { fraction = 0.5; };
+			y = { fraction = 0.5; };
+			plugins = [
+        anyrun.packages.x86_64-linux.applications
+        #anyrun.packages.x86_64-linux.websearch
+			];
 		};
 		extraCss = ''
       /*  */
+			#window {
+			  /*background-color: #000000;*/
+			  background-color: rgba(0,0,0,0.3);
+				/*opacity: 0.8;*/
+			}
+
+			#entry {
+				background-color: #2C2C2C;
+				color: #000000;
+				/*height: 40px;*/
+				border-radius: 20px;
+				border: 3px solid #ffffff;
+			}
+
+			#main {
+				background-color: #2C2C2C;
+			}
 		'';
 	};
 
