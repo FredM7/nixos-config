4026cfbec262ae96650989d54b46382d14f2c8e6
FredM7 <fred@moller.cloud>
2024-04-23 09:55:07 +0200

    noticed duplication

--- src/modules/config-loader.nix
+++ src/modules/config-loader.nix
@@ -1,27 +1,29 @@
 { pkgs, ... }: {
-  home.file.".config/hypr/" = {
-    # hypridle, hyprpaper, etc
-    source = ./../configs/hypr;
-    recursive = true;
-  };
+  home.file = {
+    ".config/hypr/" = {
+      # hypridle, hyprpaper, etc
+      source = ./../configs/hypr;
+      recursive = true;
+    };
 
-  home.file.".config/alacritty/" = {
-    source = ./../configs/alacritty;
-    recursive = true;
-  };
+    ".config/alacritty/" = {
+      source = ./../configs/alacritty;
+      recursive = true;
+    };
 
-  home.file."./.config/nvim/" = {
-    source = ./../configs/nvim;
-    recursive = true;
-  };
+    "./.config/nvim/" = {
+      source = ./../configs/nvim;
+      recursive = true;
+    };
 
-  home.file.".config/rofi/" = {
-    source = ./../configs/rofi;
-    recursive = true;
-  };
+    ".config/rofi/" = {
+      source = ./../configs/rofi;
+      recursive = true;
+    };
 
-  home.file.".config/swappy/" = {
-    source = ./../configs/swappy;
-    recursive = true;
+    ".config/swappy/" = {
+      source = ./../configs/swappy;
+      recursive = true;
+    };
   };
 }
